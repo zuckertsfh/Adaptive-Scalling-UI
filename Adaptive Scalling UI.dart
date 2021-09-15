ce1d38e393bc9ab70e9e55eae4a771282d28d1a3
Diski Tafa <41816157+zuckertsfh@users.noreply.github.com>
2021-09-15 08:22:58 +0700

    Adaptive

---
+++ Adaptive Scalling UI.dart
@@ -0,0 +1,48 @@
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static late double blockSizeHorizontal;
+  static late double blockSizeVertical;
+
+  static late double _safeAreaHorizontal;
+  static late double _safeAreaVertical;
+  static late double safeBlockHorizontal;
+  static late double safeBlockVertical;
+
+  static late bool isLandscape;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth / 100;
+    blockSizeVertical = screenHeight / 100;
+
+
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+    _safeAreaVertical =
+        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+    
+    isLandscape = (_mediaQueryData.orientation == Orientation.landscape);
+    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
+    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
+  }
+}
+
+double width(double x) {
+  return (SizeConfig.isLandscape
+          ? SizeConfig.safeBlockVertical
+          : SizeConfig.safeBlockHorizontal) *
+      x;
+}
+
+double height(double x) {
+  return (SizeConfig.isLandscape
+          ? SizeConfig.safeBlockHorizontal
+          : SizeConfig.safeBlockVertical) *
+      x;
+}
+
+bool isLandscape() => SizeConfig.isLandscape;
